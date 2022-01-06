d92c36aab7366c2e913b683cbef5759eeb07336f
Steve William <ngahanestevewilliam@gmail.com>
2022-01-06 22:18:58 +0100

    Dev

---
+++ partieldev/Views/VideoDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using partieldev.ViewModels;
+
+namespace partieldev.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
